21956bf0f6c06e6996f9fb3b1998e9c6a02326ec
JordanGallant <39-12602@sae.edu.za>
2023-09-07 01:30:08 +0200

    more-routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,40 @@ import Link from "next/link";
 const Navbar = () => {
   return (
     <div className="navbar">
-      <Image className= "logo" src= "/neon_9.png" width ={50} height ={50}alt="Picture" ></Image>
-    <nav>
-      <ul className="nav-links">
-      <li><a className = "linkies" href="/project"><Link href = "/pages/login"> Login</Link></a></li>
-      <li> <a className = "linkies" href="/login"><Link href = "/pages/register"> Register</Link></a></li>
-      <li> <a className = "linkies" href="/register"><Link href = "/pages/project/plastic"> Projects</Link></a></li>
-      </ul>
-    </nav>
-    <a className="cta" href="/contact"><button className="con-but">contact</button></a>
+      <Image
+        className="logo"
+        src="/neon_9.png"
+        width={50}
+        height={50}
+        alt="Picture"
+      ></Image>
+      <nav>
+        <ul className="nav-links">
+          <li>
+            <a className="linkies" href="/project">
+              <Link href="/pages/login"> Login</Link>
+            </a>
+          </li>
+          <li>
+            {" "}
+            <a className="linkies" href="/login">
+              <Link href="/pages/register"> Register</Link>
+            </a>
+          </li>
+          <li>
+            {" "}
+            <a className="linkies" href="/register">
+              <Link href="/project/plastic"> Projects</Link>
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <a className="cta" href="/contact">
+        <button className="con-but">
+          <Link href="/pages/contact"> Contact</Link>
+        </button>
+      </a>
     </div>
-    
   );
 };
 
---
+++ src/app/pages/contact/page.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+
+interface pageProps {}
+
+const Contact: FC<pageProps> = ({}) => {
+    return <div>Contact</div>
+}
+export default Contact
