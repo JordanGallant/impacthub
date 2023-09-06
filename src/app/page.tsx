a4aea3dd90a57c3010f62618fc5c4212a468fa4f
JordanGallant <39-12602@sae.edu.za>
2023-09-07 00:25:08 +0200

    styles-for-main-component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,25 +4,27 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-    <Head>
-    <link rel="preconnect" href="https://fonts.googleapis.com"/>
-    <link rel="preconnect" href="https://fonts.gstatic.com" />
-    <link href="https://fonts.googleapis.com/css2?family=Unbounded:wght@500&display=swap" rel="stylesheet"/>
-    </Head>
       <div className="container">
-        <h1>Impact hub</h1>
+        <div className="main">
+          <div className="left">
+            <h1>Impact hub</h1>
+            <h2>
+              Connect. Build <br />
+              Regenerate the Future. <br />
+              <br />
+            </h2>
+          </div>
 
-        <Image
-          src="/neon_9.png"
-          width={500}
-          height={500}
-          alt="Picture of the author"
-        />
-        <h2>
-          Connect. Build <br />
-          Regenerate the Future. <br />
-          <br />
-        </h2>
+          <div className="right">
+
+          <Image
+            src="/neon_9.png"
+            width={350}
+            height={350}
+            alt="Picture of the author"
+          />
+          </div>
+        </div>
       </div>
     </>
   );
