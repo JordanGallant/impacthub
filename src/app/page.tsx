a0e1765f30d89a804fc841e2f6e474a1f495758c
JordanGallant <39-12602@sae.edu.za>
2023-09-07 01:10:15 +0200

    creating-navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
 import Image from "next/image";
 import Head from "next/head";
 import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <Image className= "logo" src= "/neon_9.png" width ={50} height ={50}alt="Picture" ></Image>
+    <nav>
+      <ul className="nav-links">
+      <li><a href="/project">Projects</a></li>
+      <li> <a href="/login">Login</a></li>
+      <li> <a href="/register">Register</a></li>
+      </ul>
+    </nav>
+    <a className="cta" href="/contact"><button>contact</button></a>
+    </div>
+    
+  );
+};
+
 export default function Home() {
   return (
     <>
+      <Navbar />
       <div className="container">
         <div className="main">
           <div className="left">
@@ -16,13 +35,7 @@ export default function Home() {
           </div>
 
           <div className="right">
-
-          <Image
-            src="/neon_9.png"
-            width={350}
-            height={350}
-            alt="Picture of the author"
-          />
+            <Image src="/neon_9.png" width={350} height={350} alt="Picture" />
           </div>
         </div>
       </div>
