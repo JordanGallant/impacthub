37055b2df35ed06b71869adfeecbf150c0a773e8
JordanGallant <39-12602@sae.edu.za>
2023-09-07 06:43:23 +0200

    adding-curved-styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,21 +15,22 @@ const Navbar = () => {
       <nav>
         <ul className="nav-links">
           <li>
-            <a className="linkies" href="/project">
-              <Link href="/pages/login"> Login</Link>
-            </a>
+            <Link className="linkies" href="/pages/login">
+              {" "}
+              Login
+            </Link>
           </li>
           <li>
-            {" "}
-            <a className="linkies" href="/login">
-              <Link href="/pages/register"> Register</Link>
-            </a>
+            <Link className="linkies" href="/pages/register">
+              {" "}
+              Register
+            </Link>
           </li>
           <li>
-            {" "}
-            <a className="linkies" href="/register">
-              <Link href="/project/plastic"> Projects</Link>
-            </a>
+            <Link className="linkies" href="/project/plastic">
+              {" "}
+              Projects
+            </Link>
           </li>
         </ul>
       </nav>
@@ -60,6 +61,22 @@ export default function Home() {
           <div className="right">
             <Image src="/neon_9.png" width={350} height={350} alt="Picture" />
           </div>
+          <div className="wave">
+            <svg
+              data-name="Layer 1"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 1200 120"
+              preserveAspectRatio="none"
+            >
+              <path
+                d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+                className="shape-fill"
+              ></path>
+            </svg>
+          </div>
+        </div>
+        <div className="projects">
+          <h2>Projects</h2>
         </div>
       </div>
     </>
