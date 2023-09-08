6ce5bed707171bd177cd6d70ef12f398ff3956b8
JordanGallant <39-12602@sae.edu.za>
2023-09-08 17:13:07 +0200

    changes

--- src/app/pages/stats/page.tsx
+++ src/app/pages/stats/page.tsx
@@ -1,8 +1,36 @@
-import { FC } from "react";
+"use client";
+import WorldMap from "react-svg-worldmap";
 
-interface pageProps {}
 
-const Stats: FC<pageProps> = ({}) => {
-    return <div>Stats</div>
+
+
+function Stats() {
+  const data = [
+    // russia
+    { country: "es", value: 1 }, // Spain
+    { country: "za", value: 1 }, 
+    { country: "jo", value: 1 },//South Africa
+  ];
+
+  return (
+    <>
+      <div className="stat-div">
+        <div className="map">
+          <WorldMap
+            color="red"
+            title="Top 10 Populous Countries"
+            value-suffix="people"
+            size="xl"
+            data={data}
+          />
+        </div>
+        <div className="stats-data">
+          <h1 className="stat-title">Dashboard</h1>
+          
+        </div>
+      </div>
+    </>
+  );
 }
-export default Stats
+
+export default Stats;
