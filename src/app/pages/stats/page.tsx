dec23460ee086cdc67853a8423e26c1785257e5a
JordanGallant <39-12602@sae.edu.za>
2023-09-08 17:15:43 +0200

    first commit

--- src/app/pages/stats/page.tsx
+++ src/app/pages/stats/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import WorldMap from "react-svg-worldmap";
-
+import Image from "next/image";
 
 
 
@@ -8,7 +8,7 @@ function Stats() {
   const data = [
     // russia
     { country: "es", value: 1 }, // Spain
-    { country: "za", value: 1 }, 
+    { country: "za", value: 2 }, 
     { country: "jo", value: 1 },//South Africa
   ];
 
@@ -19,13 +19,17 @@ function Stats() {
           <WorldMap
             color="red"
             title="Top 10 Populous Countries"
-            value-suffix="people"
+            value-suffix="Projects"
             size="xl"
             data={data}
           />
         </div>
         <div className="stats-data">
           <h1 className="stat-title">Dashboard</h1>
+          <div className="image-div">
+          <Image src="/pie2.jpeg" width={350} height={350} alt="Picture" />
+          </div>
+          
           
         </div>
       </div>
